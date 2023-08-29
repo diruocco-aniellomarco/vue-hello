b6da0d6965dbb93163e190a26b4582c96926be2e
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-08-29 15:00:53 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Ciao Prof!'
+        message: 'Ciao Prof!',
+        immagine: 'https://picsum.photos/200'
       }
     }
   }).mount('#app')
